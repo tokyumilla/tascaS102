d834bc061de27ff38d975f77fc64f2dce0cce327
tokyumilla <tokyumilla@gmail.com>
2022-10-30 23:27:18 +0100

    tengo un lío tremendo

--- src/n1exercici1/Sale.java
+++ src/n1exercici1/Sale.java
@@ -6,13 +6,16 @@ public class Sale {
     private ArrayList <Product> productList;
     private float totalCost;
 
-    public void calculateTotal() {
+    public void calculateTotal() throws EmptySaleException {
         try {
             float total = 0;
             for (int i = 0; i < productList.size() ; i++) {
                 total += productList.get(i).getPrice();
 
             }
+        } catch (EmptySaleException) {
+
+//("Para hacer una venta primero tienes que añadir productos")
         }
 
     }
